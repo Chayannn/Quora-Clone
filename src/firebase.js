2d9b4a32ad6fc36f21a89589331c8d8c11fa40ff
Chayannn <chayanp188@gmail.com>
2023-05-03 20:37:30 +0530

    added firebase hosting

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBdBWKPVku7Kzgk8LOiQmSMdvWgcHBf2I0',
-  authDomain: 'linkedin-clone-354e6.firebaseapp.com',
-  projectId: 'linkedin-clone-354e6',
-  storageBucket: 'linkedin-clone-354e6.appspot.com',
-  messagingSenderId: '1072906921269',
-  appId: '1:1072906921269:web:37cb1c171820da2c13987c',
+  apiKey: "AIzaSyDPk_1cMRJa6yttGWMlYCLrbKJbvah9Nds",
+  authDomain: "quora-clone-f6314.firebaseapp.com",
+  projectId: "quora-clone-f6314",
+  storageBucket: "quora-clone-f6314.appspot.com",
+  messagingSenderId: "814522325073",
+  appId: "1:814522325073:web:5253a12036f3a7a9d58070"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
